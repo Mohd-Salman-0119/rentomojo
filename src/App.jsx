c23f4291e1451f4c6410674da386d6136f3741bb
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-02-08 11:55:55 +0530

    20% work done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import Homepage from "./screen/Homepage";
+
 
 const App = () => {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return(
+       <Homepage/>
+    )
 };
 
 export default App;
--- src/screen/Homepage.jsx
+++ src/screen/Homepage.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import { BsFillChatQuoteFill } from "react-icons/bs";
+import Crousel from "../components/Crousel";
+import PageLink from "../components/PageLink";
+
+// import Navbar from "../components/Navbar";
+
+
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>Welcome to Home page</h1>
-    </div>
-  )
-}
+    <Box>
+      {/* <Navbar /> */}
+      <Box>
+        <Box width={"90%"} margin={"auto"}>
+          <Crousel/>
+          <PageLink/>
+        </Box>
+        <Box
+          bg={"rgb(245,247,250)"}
+          border="1px solid rgb(245,247,250)"
+          marginTop="-50px"
+        >
+          {/* <SliderHome />
+          <MyOwlCarousel /> */}
+        </Box>
+        {/* <Features />
+        <Reviews /> */}
+        {/* chat icon */}
+        {/* <Footer /> */}
+      </Box>
+    
+    </Box>
+  );
+};
 
-export default Homepage
+export default Homepage;
